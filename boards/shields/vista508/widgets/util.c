55173b3b4d9a5ca18a8c20cb842f00d1a7fb5c4f
MickiusMousius <MickiusMousius@icloud.com>
2025-06-30 20:49:32 +0930

    Batteries show percentages nicely now

--- boards/shields/vista508/widgets/util.c
+++ boards/shields/vista508/widgets/util.c
@@ -25,7 +25,7 @@ void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
 }
 
 /*
-void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
+void draw_battery_info(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lv_font_montserrat_16, LV_TEXT_ALIGN_LEFT);
 
